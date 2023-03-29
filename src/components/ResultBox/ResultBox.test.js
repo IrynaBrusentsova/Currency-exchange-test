f089dcb9ce4a7ce903375259ba1cfe04d2d564e1
Iryna Brusentsova <bio200480@gmail.com>
2023-03-29 11:30:50 +0300

    Fix test in ResultBox

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -9,7 +9,7 @@ const testCases = [
     { amount: '345', from: 'USD', to: 'PLN' },
 ];
 
-for(const testObj of testCases){
+
 
   describe('Component ResultBox', () => {
 
@@ -17,17 +17,40 @@ for(const testObj of testCases){
         render(<ResultBox from="PLN" to="USD" amount={100} />);
     });
 
+    // it ('should render proper info about conversion when PLN -> USD', ()=>{
+    //     render(<ResultBox from="PLN" to="USD" amount={Number(testObj.amount)} />);
+    //     const output = screen.getByTestId('output');
+    //     expect(output).toHaveTextContent('PLN 100.00 = $28.57');  
+    // });
+    
+
     it ('should render proper info about conversion when PLN -> USD', ()=>{
-        render(<ResultBox from="PLN" to="USD" amount={Number(testObj.amount)} />);
+      for (testObj of testCases){
+          
+        render(<ResultBox from="PLN" to="USD" amount= {Number(testObj.amount)} />);
         const output = screen.getByTestId('output');
-        expect(output).toHaveTextContent('PLN 100.00 = $28.57');  
+
+        expect(output).toHaveTextContent(`$${Number(testObj.amount)}.00 = PLN ${Number(testObj.amount)*3.5}.00`);
+
+        cleanup();
+      }
     });
 
+
     it ('should render proper info about conversion when USD -> PLN', () => {
-        render(<ResultBox from="USD" to="PLN" amount={Number(testObj.amount)} />);
+       for (testObj of testCases){
+          
+        render(<ResultBox from="USD" to="PLN" amount= {Number(testObj.amount)} />);
         const output = screen.getByTestId('output');
-        expect(output).toHaveTextContent('USD 100.00 = $350');
-    });
+
+        expect(output).toHaveTextContent(`PLN ${ Number(testObj.amount)}.00 = $${
+          Math.round((Number(testObj.amount) / 3.5) * 100)/100
+        }`
+        );
+        
+        cleanup();
+      }
+    });   
 
     
     it ('should render proper info about conversion when PLN 123.00 -> PLN 123.00', () =>{
@@ -45,7 +68,8 @@ for(const testObj of testCases){
     cleanup()   
 });
 
-}
+
+
 
 
 
